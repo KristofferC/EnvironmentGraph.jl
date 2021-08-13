0c72865ff7b1555f74eb2661660654c584997ad6
KristofferC <kcarlsson89@gmail.com>
2021-08-13 11:22:39 +0200

    add code

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # EnvironmentGraph
+
+```jl
+(EnvironmentGraph) pkg> activate --temp
+  Activating new project at `/tmp/jl_mG82U9`
+
+(jl_mG82U9) pkg> add MKL
+   Resolving package versions...
+    Updating `/tmp/jl_mG82U9/Project.toml`
+  [33e6dc65] + MKL v0.4.1
+
+julia> g = EnvironmentGraph.environment_graph("/tmp/jl_mG82U9/")
+{39, 100} directed Int64 metagraph with Float64 weights defined by :weight (default weight 1.0)
+
+julia> EnvironmentGraph.print_cycles(g)
+Pkg -> p7zip_jll↫
+Pkg -> Downloads -> LibCURL -> MozillaCACerts_jll↫
+Pkg -> Downloads -> LibCURL -> LibCURL_jll↫
+Pkg -> Downloads -> LibCURL -> LibCURL_jll -> LibSSH2_jll↫
+Pkg -> Downloads -> LibCURL -> LibCURL_jll -> LibSSH2_jll -> MbedTLS_jll↫
+Pkg -> Downloads -> LibCURL -> LibCURL_jll -> MbedTLS_jll↫
+Pkg -> Downloads -> LibCURL -> LibCURL_jll -> nghttp2_jll↫
+Pkg -> Downloads -> LibCURL -> LibCURL_jll -> Zlib_jll↫
+```
--- src/EnvironmentGraph.jl
+++ src/EnvironmentGraph.jl
@@ -1,5 +1,49 @@
 module EnvironmentGraph
 
-# Write your package code here.
+using LightGraphs, MetaGraphs
+using TOML
+using Pkg
+using UUIDs
+
+function environment_graph(env::String)
+    manifest_file = joinpath(env, "Manifest.toml")
+    if !isfile(manifest_file)
+       error("expected a `Manifest.toml` file at $(abspath(env))")
+    end
+    project_file = joinpath(env, "Project.toml")
+    if !isfile(project_file)
+        error("expected a `Project.toml` file at $(abspath(env))")
+    end
+    manifest = Pkg.Types.read_manifest(manifest_file)
+    project = Pkg.Types.read_project(project_file)
+
+    n_deps = length(manifest.deps)
+    d = Dict{UUID, Int}()
+    for (i, (uuid, _)) in enumerate(manifest.deps)
+        d[uuid] = i
+    end
+
+    g = MetaDiGraph(LightGraphs.SimpleGraphs.SimpleDiGraph(n_deps))
+
+    for (uuid, pkg) in manifest.deps
+        props = Dict(:name => pkg.name, :version => pkg.version, :uuid => uuid)
+        set_props!(g, d[pkg.uuid], props)
+        for (dep_name, uuid) in pkg.deps
+            add_edge!(g, d[pkg.uuid], d[uuid])
+        end
+    end
+
+    return g
+end
+
+print_cycles(g) = print_cycles(stdout, g)
+function print_cycles(io::IO, g)
+    cycles = LightGraphs.simplecycles(g)
+    for c in cycles
+        join(io, [props(g, p)[:name] for p in c], " -> ")
+        print(io, "↫")
+        println()
+    end
+end
 
 end
