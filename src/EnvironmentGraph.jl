9ecb25acbc28c279772edf7195797dd6a1fda96b
Kristoffer <kcarlsson89@gmail.com>
2023-05-20 20:57:40 +0200

    improvements for extensions

--- src/EnvironmentGraph.jl
+++ src/EnvironmentGraph.jl
@@ -15,25 +15,63 @@ function environment_graph(env::String)
         error("expected a `Project.toml` file at $(abspath(env))")
     end
     manifest = Pkg.Types.read_manifest(manifest_file)
-    project = Pkg.Types.read_project(project_file)
 
+    # First we need to map UUIDs to integers
+    # Start with normal dependencies
     n_deps = length(manifest.deps)
+    uuid_to_name = Dict{UUID, String}()
     d = Dict{UUID, Int}()
-    for (i, (uuid, _)) in enumerate(manifest.deps)
+    i = 1
+    for (uuid, entry) in manifest.deps
         d[uuid] = i
+        uuid_to_name[uuid] = entry.name
+        i += 1
     end
 
-    g = MetaDiGraph(Graphs.SimpleGraphs.SimpleDiGraph(n_deps))
+    # Then add possible extensions
+    for (uuid, entry) in manifest.deps
+        for (ext, _) in entry.exts
+            uuid_ext = Base.uuid5(uuid, ext)
+            d[uuid_ext] = i
+            i += 1
+        end
+    end
+
+    g = MetaDiGraph(Graphs.SimpleGraphs.SimpleDiGraph(i-1))
 
     for (uuid, pkg) in manifest.deps
-        props = Dict(:name => pkg.name, :version => pkg.version, :uuid => uuid)
+        props = Dict(:label => pkg.name, :name => pkg.name, :version => pkg.version, :uuid => uuid, :extension => false)
         du = d[pkg.uuid]
         set_props!(g, du, props)
-        for (dep_name, uuid) in pkg.deps
+        for (_, uuid) in pkg.deps
             add_edge!(g, du, d[uuid])
         end
-        isdefined(pkg, :weakdeps) && for (dep_name, uuid) in pkg.weakdeps
-            haskey(d, uuid) && add_edge!(g, du, d[uuid])
+    end
+
+    for (uuid, pkg) in manifest.deps
+        # Lookup UUIDs of weak deps
+        for (ext, triggers) in pkg.exts
+            triggers isa String && (triggers = [triggers])
+            all_triggers_present = true
+            for trigger in triggers
+                uuid_trigger = pkg.weakdeps[trigger]
+                if !haskey(d, uuid_trigger)
+                    all_triggers_present = false
+                    break
+                end
+            end
+            all_triggers_present || continue
+            uuid_ext = Base.uuid5(uuid, ext)
+            du = d[uuid_ext]
+            props = Dict(:label => ext, :name => ext, :version => nothing, :uuid => uuid_ext, :extension => true)
+            set_props!(g, du, props)
+            add_edge!(g, du, d[pkg.uuid])
+            for trigger in triggers
+                uuid_trigger = pkg.weakdeps[trigger]
+                if haskey(d, uuid_trigger)
+                    add_edge!(g, du, d[uuid_trigger])
+                end
+            end
         end
     end
 
